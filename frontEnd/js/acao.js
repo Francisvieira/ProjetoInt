cdc02be63e29750316bf129051433e25014e9720
francisco <fvieira340@gmail.com>
2022-02-06 20:50:28 -0300

    primeiro commit para a aula

---
+++ frontEnd/js/acao.js
@@ -0,0 +1,42 @@
+
+
+function carregarDadosAPI() {
+    fetch(`http://localhost:3000/produtos/listar`)
+        .then((response) => response.json())
+        .then((resultado) => {
+            console.log(resultado)
+            var linha = ``;
+
+            resultado.output.map((itens, ix) => {
+                linha += `<div class="card">
+                <img src=${itens.foto} class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${itens.nomeproduto}</h5>
+                  <p class="card-text">${itens.descricao}</p>
+                  <p class="card-text">${itens.preco}</p>
+                  <p class="card-text"><small class="text-muted"><button type="button" class="btn btn-primary btn-lg">Comprar</button>
+                  
+                </small></p>
+                </div>
+              </div> `;
+            });
+           
+         
+            document.getElementById("grupocard").innerHTML = linha;
+
+        })
+        .catch((erro) => console.error(`Erro ao carregar a API->${erro}`))
+}
+function carregarAtualizar() {
+    var id = window.location.search.substring(4);
+    fetch(`http://localhost:3000/produto/buscar/${id}`)
+        .then((response) => response.json())
+    then((dados) => {
+        console.log(dados);
+        document.getElementById("txtNomeProduto").value = dados.output[0].nomeproduto;
+        document.getElementById("txtDescricao").value = dados.output[0].descricao;
+        document.getElementById("txtPreco").value = dados.output[0].preco;
+        document.getElementById("txtFoto").value = dados.output[0].foto;
+    })
+        .catch((erro) => console.error(`Erro ao carregar a api -> ${erro}`));
+}
---
+++ index.js
@@ -0,0 +1,91 @@
+//******************Backend 
+
+const mysql = require("mysql");
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//***************Banco de dados ****************
+const interacao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bdprodutos"
+})
+interacao.connect((erro) => {
+    if (erro) {
+        console.error("erro ao tentar estabelecer a conexao" + erro.stack);
+        return;
+    }
+    console.log("conectado ao banco de dados ->" + interacao.threadId);
+})
+
+
+app.get("/produtos/listar", (req, res) => {
+    //consulta sql para selecionar os produtos no DB
+    interacao.query("select * from tbproduto", (erro, resultado) => {
+        if (erro) {
+            return res.status(500).send({ output: "erro ao tentar executar a consulta" + erro });
+        }
+        res.status(200).send({ output: resultado });
+    });
+
+});
+
+//**********************buscar o produto
+
+app.get("/produto/buscar/:id", (req, res) => {
+    interacao.query("select * from tbproduto where idproduto=?", [req.params.id], (erro, resultado) => {
+        if (erro) {
+            return res.status(500).send({ output: `Erro ao tentar executar a consulta ${erro}` });
+        }
+        if (resultado == null || resultado == "") {
+            return res.status(404).send({ output: `Não foi possivel localizar esse produto` });
+
+        }
+        res.status(200).send({ output: resultado });
+    });
+})
+
+app.post("/produto/cadastro", (req, res) => {
+    interacao.query("insert into tbproduto set ?", [req.body], (erro, resultado) => {
+        if (erro) {
+            res.status(500).send({ output: `Não foi possivel cadastrar -> ${erro}` });
+            return;
+        }
+        res.status(201).send({ output: resultado });
+    });
+});
+
+app.put("/produto/atualizar/:id", (req, res) => {
+    console.log(req)
+    interacao.query("update tbproduto set ? where idproduto=?", [req.body, req.params.id], (erro, resultado) => {
+        if (erro) {
+            res.status(500).send({ output: `Erro ao tentar atualizar os dados -> ${erro}` });
+            return;
+        }
+        res.status(200).send({ output: resultado });
+    });
+
+});
+
+
+//***************** * Deletar*************************
+app.delete("/produto/apagar/:id", (req, res) => {
+    interacao.query("delete from tbproduto where idproduto =?", [req.params.id], (erro, resultado) => {
+        if (erro) {
+            res.status(500).send({ output: `Erro ao tenatar apagar o produto ->${erro}` });
+            return;
+        }
+        res.status(204).send({ output: resultado });
+    });
+});
+
+// subir o servidor na porta 2000 
+app.listen("3000", () => console.log("Servidor online em: http://localhost:3000"));
+
+module.exports = app;
